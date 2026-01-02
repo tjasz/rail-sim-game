7bbdbec0d6c2166aa0d64b69e2b3e18e2e4c1211
tjasz <nukorausa@hotmail.com>
2026-01-01 17:08:11 -0800

    Give citizens a key

--- src/components/CitizenMarkers.tsx
+++ src/components/CitizenMarkers.tsx
@@ -4,7 +4,7 @@ import type { Citizen, Neighborhood } from '../models';
 export const renderCitizenIcon = (position: [number, number], size: number, citizen: Citizen, neighborhoods: Map<string, Neighborhood>) => {
   const fill = "black";
   const destinationNeighborhoodIcon = neighborhoods.get(citizen.destinationNeighborhoodId)?.icon ?? 'circle';
-  return <g transform={`translate(${position[0]}, ${position[1]}) scale(${size / 15})`}>
+  return <g key={citizen.id} transform={`translate(${position[0]}, ${position[1]}) scale(${size / 15})`}>
       <path
         fill={fill}
         opacity="0.8"
